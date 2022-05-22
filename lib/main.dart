4200001726d88382bfea3156decf91e12e17e77a
Youhei Oonuma <oonuma617@gmail.com>
2022-05-22 17:39:57 +0900

    crashlytics

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await FirebaseCrashlytics.instance
+      .setCrashlyticsCollectionEnabled(kDebugMode);
+  Function originalOnError = FlutterError.onError!;
+
+  FlutterError.onError = (errorDetails) async {
+    await FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
+    originalOnError(errorDetails);
+  };
+
+  runZonedGuarded(() {
+    runApp(const MyApp());
+  }, (error, stackTrace) {
+    FirebaseCrashlytics.instance.recordError(error, stackTrace);
+  });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            children: [
+              const Text('test'),
+              ElevatedButton(
+                onPressed: () => throw Exception('uncatch exception'),
+                child: const Text("Throw Test Exception"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
